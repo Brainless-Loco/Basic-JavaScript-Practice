2f375fdaf0fdc2d9f415938f76f0388b877e0210
Tonmoy Das <71629393+Brainless-Loco@users.noreply.github.com>
2021-02-06 01:10:08 +0600

    Add files via upload

--- Tiny-Friend-Name.js
+++ Tiny-Friend-Name.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Here goes a function to return the smallest name from all of your friend names list.
 
 function tinyFriend(friendNames){
@@ -14,4 +15,22 @@ function tinyFriend(friendNames){
 }
 
 let aList = [] ;
+=======
+// Here goes a function to return the smallest name from all of your friend names list.
+
+function tinyFriend(friendNames){
+    if(friendNames.length==0){
+        return "Sorry to hear that you have no friends at all! :(";
+    }
+    let tinyFriendName = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    for(var i = 0 ; i< friendNames.length; i++){
+        if(friendNames[i].length < tinyFriendName.length){
+            tinyFriendName = friendNames[i];
+        }
+    }
+    return "Your Tiny Friend among all Friends is none but " + tinyFriendName + ".";
+}
+
+let aList = [] ;
+>>>>>>> 72dc8de (pushing)
 console.log(tinyFriend(aList));
--- factorial.js
+++ factorial.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Here goes a function to measure factorial of a number iteretively
 
 function factorial(number){
@@ -11,4 +12,19 @@ function factorial(number){
     return "Factorial of " + number + " is " + factorialAns; 
 }
 
+=======
+// Here goes a function to measure factorial of a number iteretively
+
+function factorial(number){
+    if(number < 0){
+        return "Negative number's factorial can't be measured.";
+    }
+    let factorialAns = 1;
+    for(var i=1 ; i<= number ; i++){
+        factorialAns *= i;
+    }
+    return "Factorial of " + number + " is " + factorialAns; 
+}
+
+>>>>>>> 72dc8de (pushing)
 console.log(factorial(5));
